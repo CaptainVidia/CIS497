de88e5761745637b12544678d1271af75b4f1fa5
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-09-10 09:43:35 -0500

    p2

---
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    private HealthSystem healthSystemScript;
+
+    private void Start()
+    {
+        healthSystemScript = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+    }
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < bottomBound)
+        {
+            //Debug.Log("Game Over!");
+            //grab the health system script and call the TakeDamage ()
+            healthSystemScript.TakeDamage();
+
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+
+    private float leftBound = -14;
+    private float rightBound = 14;
+    private float spawnPosZ = 20;
+    public bool gameOver = false;
+    public HealthSystem healthSystem;
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.S))
+    //    {
+    //        SpawnRandomPrefab();
+    //    }
+    //}
+
+    void SpawnRandomPrefab()
+    {
+        //generate index
+        int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+        //generate spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        //spawn
+        Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+    }
+
+    void Start()
+    {
+        //getting reference to health system script
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+
+        //calling methold repeatedly ("method", startDelay, spawnInterval)
+        //InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+        StartCoroutine(SpawnRandomPrefabwithCoroutine());
+
+    }
+
+    IEnumerator SpawnRandomPrefabwithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!healthSystem.gameOver)
+        {
+            SpawnRandomPrefab();
+
+            //float randomDelay = Random.Range(0.8f, 2.0f);
+
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+}
