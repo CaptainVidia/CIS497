3eb236c4fa5c0529c190b722fe6f4926e1cdaaf7
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-11-07 12:26:53 -0600

    Proto Working

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    public int enemyCount;
+    public static int waveNumber = 1;
+    public GameObject powerupPrefab;
+    public Text winText;
+    public Text waveText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNumber = 1;
+        waveText.text = "Wave: " + waveNumber;
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup(1);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            waveText.text = "Wave: " + waveNumber;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup(1);
+        }
+
+        if (waveNumber == 10)
+        {
+
+           winText.text = "You Win Press R to restart!";
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void SpawnPowerup(int powerupsToSpawn)
+    {
+        for (int i = 0; i < powerupsToSpawn; i++)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+}
