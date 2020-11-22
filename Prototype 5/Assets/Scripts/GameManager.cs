14ca646c3a709d2ac0da42ea134d2c87825520d7
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-11-22 07:52:21 -0600

    Prototype 5

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    // Start is called before the first frame update
+    public TextMeshProUGUI scoreText;
+    private int score;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Start()
+    {
+        
+       
+        
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            //wait for 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //pick a random index b/w 0 and number of prefabs
+            int index = Random.Range(0, targets.Count);
+
+            //spawn the prefab at the randomly selected index
+            Instantiate(targets[index]);
+
+            // test updateScore by adding 5 every time a new target
+            //UpdateScore(5);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+}
