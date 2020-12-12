2b79db32d79b152552082e9c964d9cf2aa5434dc
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-12-12 03:57:18 -0600

    Assigment 9 Finished

---
+++ Assignment9NavMesh/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public GameObject player;
+    public float chaseDistance;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        character = GetComponent<ThirdPersonCharacter>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseDistance = 5.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        float distanceFromTarget = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceFromTarget > agent.stoppingDistance && distanceFromTarget < chaseDistance)
+        {
+            agent.SetDestination(player.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
---
+++ Assignment9NavMesh/Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
